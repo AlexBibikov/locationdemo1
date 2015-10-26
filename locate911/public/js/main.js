d5d385c824b87e0c5f808d9373d1947a7bb6cb2c
AlexBibikov <a_bibikov@yahoo.com>
2015-10-26 13:30:27 -0500

    first commit

---
+++ locate911/public/js/main.js
@@ -0,0 +1,71 @@
+﻿var map;
+
+$(document).ready(function () {
+    
+    function addPoint(map, coord, title, circleColor) {
+        map.setView([coord.latitude, coord.longitude], 18);
+        var marker = L.marker(
+            [coord.latitude, coord.longitude],
+            { title: title }).addTo(map);
+        
+        var addrUrl = "http://nominatim.openstreetmap.org/reverse?format=json&lat=LAT&lon=LON&zoom=18&addressdetails=1".replace("LAT", coord.latitude).replace("LON", coord.longitude);
+        jQuery.getJSON(addrUrl, function (data, b, c) {
+            var html = "<h1>" + title + "</h1>";
+            html += "<div><strong>Address:</strong></div>";
+            html += "<div><i>" + data.display_name + "</i></div>";
+            marker.bindPopup(html, {});
+        });
+
+        var circle = L.circle(
+            [coord.latitude, coord.longitude], 
+            coord.accuracy, 
+            { 'color': circleColor || '#f30' }).addTo(map);
+        circle.marker = marker;
+        return circle;
+    }
+
+    function createMap() {
+        map = L.map('map');
+        
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png?{foo}', { foo: 'bar' }).addTo(map);
+        
+        var nexrad = L.tileLayer.wms("http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi", {
+            layers: 'nexrad-n0r-900913',
+            format: 'image/png',
+            transparent: true,
+            opacity: 0.5,
+            attribution: "Weather data © 2012 IEM Nexrad"
+        }).addTo(map);
+/*        
+        var nexrad = L.tileLayer.wms("http://mesonet.agron.iastate.edu/wfs/ww.php", {
+            layers: 'nexrad-n0r-900913',
+            format: 'image/png',
+            transparent: true,
+            attribution: "Weather data © 2012 IEM Nexrad"
+        }).addTo(map);
+*/
+        return map;
+    }
+
+    navigator.geolocation.getCurrentPosition(function (p) {
+        var map = createMap();
+        var point = addPoint(map, p.coords, "My Location", '#03f');
+    }, function (err) { 
+        console.log("getCurrentPosition error: ", err.message);
+    }, {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    });
+
+    $('#get-location-btn').click(function () {
+        var val = $('#phone-no', this.parentElement).val();
+        console.log("#get-location-btn click : " + val);
+        $.get("/locations?p=" + val, function (r) {
+            console.log(r.coord);
+            if (r.coord && r.coord.latitude && r.coord.longitude) {
+                addPoint(map, r.coord, val + " Location", '#f30');
+            }
+        });
+    });
+});
---
+++ locate911/routes/index.js
@@ -0,0 +1,93 @@
+﻿var express = require('express');
+var router = express.Router();
+var needLocation = [];
+var ID = 1;
+var locations = [{ id: 2, phone: 2145970489}];
+var timerId;
+
+function getRecordById(id) {
+    return locations.filter(function (d) {
+        return d.id == id;
+    });
+}
+
+function getRecordByPhone(phone) {
+    return locations.filter(function (d) {
+        return d.phone == phone;
+    });
+}
+
+router.get('/locations', function (req, res) {
+    var p = req.query.p;
+    if (p) {
+        try {
+            var record = getRecordByPhone(+p);
+            if (record.length) {
+                needLocation.push(record[0].id);
+                res.json(record[0]);
+            }
+            else {
+                res.json({ found: "none" });
+            }
+        } catch (e) {
+            res.json(e);
+        }
+    }
+    else {
+        res.json(locations);
+    }
+});
+
+router.post('/location/:id', function (req, res) {
+    var record = getRecordById(req.params.id);
+    if (record.length) {
+        record[0].coord = req.body;
+        res.json(record[0]);
+    }
+    else {
+        res.json({ 'not': 'found' });
+    }
+});
+
+router.get('/register/:phone', function (req, res) {
+    ID += 1;
+    var record = { id: ID, phone: req.params.phone };
+    res.send("OK, " + ID);
+});
+
+
+router.get('/stream', function (req, res) {
+    console.log('stream');
+
+    req.setTimeout(9999999999999);
+    
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream',
+        'Cache-Control': 'no-cache',
+        'Connection': 'keep-alive'
+    });
+
+    res.write('\n');
+    
+    req.on("close", function () {
+        clearInterval(timerId);
+        console.log("clearInterval(" + timerId + ");");
+    });
+    
+    timerId = setInterval(function () {
+        while (needLocation.length > 0) {
+            console.log("needLocation[0] is " + needLocation[0]);
+            res.write("data: " + needLocation[0] + "\n\n");
+            needLocation.shift();
+        }
+    }, 1000);
+    
+    console.log("setInterval() returns " + timerId);
+});
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Location' });
+});
+
+module.exports = router;
